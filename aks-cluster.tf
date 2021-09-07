68494c277a7977a97be242c421b6e477873a2a99
Lubert Palacios <lubert@MBP-Lubert.local>
2021-09-08 00:20:36 +0200

    Initial commit

---
+++ aks-cluster.tf
@@ -0,0 +1,41 @@
+resource "random_pet" "prefix" {}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "default" {
+  name     = "${random_pet.prefix.id}-rg"
+  location = var.region
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "default" {
+  name                = "${random_pet.prefix.id}-aks"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  dns_prefix          = "${random_pet.prefix.id}-k8s"
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 2
+    vm_size         = var.node_size
+    os_disk_size_gb = 30
+  }
+
+  service_principal {
+    client_id     = var.client_id
+    client_secret = var.client_secret
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  tags = {
+    environment = var.environment
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "client_id" {
+  description = "Azure Kubernetes Service Cluster service principal"
+}
+
+variable "client_secret" {
+  description = "Azure Kubernetes Service Cluster secret"
+}
+
+variable "region" {
+  description = "Azure Region"
+  default = "westeurope"
+}
+
+variable "environment" {
+  description = "Working enviroment"
+  default = "dev"
+}
+
+variable "node_size" {
+  description = "Size for Azure instances to be used as K8s nodes"
+  default = "Standard_D2_v2"
+}
