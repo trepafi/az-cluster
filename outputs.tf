7b2047c22307813488c0821696007728adaab55c
Lubert Palacios <lubert@MBP-Lubert.local>
2021-09-10 23:52:35 +0200

    Removing posible sensitive data

--- outputs.tf
+++ outputs.tf
@@ -18,9 +18,9 @@ output "host" {
 #   value = azurerm_kubernetes_cluster.default.kube_config.0.client_certificate
 # }
 
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.default.kube_config_raw
-}
+# output "kube_config" {
+#   value = azurerm_kubernetes_cluster.default.kube_config_raw
+# }
 
 # output "cluster_username" {
 #   value = azurerm_kubernetes_cluster.default.kube_config.0.username
