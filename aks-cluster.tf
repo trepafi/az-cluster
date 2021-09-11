7b8c33bbd5a17a658298c1716846d8553ce92d5d
Lubert Palacios <lubert@MBP-Lubert.local>
2021-09-11 19:40:06 +0200

    Adding TF backend and remote state

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -1,26 +1,8 @@
-# resource "random_pet" "prefix" {}
-
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "default" {
-  # name     = "${random_pet.prefix.id}-rg"
-  name     = "${var.k8s_base_name}-rg"
-  location = var.region
-
-  tags = {
-    environment = var.environment
-  }
-}
-
 resource "azurerm_kubernetes_cluster" "default" {
-  name = "${var.k8s_base_name}-aks"
-  # name                = "${random_pet.prefix.id}-aks"
+  name                = "${var.k8s_base_name}-aks"
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
   dns_prefix          = "${var.k8s_base_name}-k8s"
-  # dns_prefix          = "${random_pet.prefix.id}-k8s"
 
   default_node_pool {
     name            = "default"
