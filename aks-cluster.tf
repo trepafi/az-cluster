4bcb9b1c85235cc7e6724e7f995953e65e4d736d
Lubert Palacios <lubert@MBP-Lubert.local>
2021-09-11 02:50:16 +0200

    Updating base name for k8s resources

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -1,11 +1,12 @@
-resource "random_pet" "prefix" {}
+# resource "random_pet" "prefix" {}
 
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "default" {
-  name     = "${random_pet.prefix.id}-rg"
+  # name     = "${random_pet.prefix.id}-rg"
+  name     = "${var.k8s_base_name}-rg"
   location = var.region
 
   tags = {
@@ -14,10 +15,12 @@ resource "azurerm_resource_group" "default" {
 }
 
 resource "azurerm_kubernetes_cluster" "default" {
-  name                = "${random_pet.prefix.id}-aks"
+  name                = "${var.k8s_base_name}-aks"
+  # name                = "${random_pet.prefix.id}-aks"
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
-  dns_prefix          = "${random_pet.prefix.id}-k8s"
+  dns_prefix          = "${var.k8s_base_name}-k8s"
+  # dns_prefix          = "${random_pet.prefix.id}-k8s"
 
   default_node_pool {
     name            = "default"
--- variables.tf
+++ variables.tf
@@ -20,3 +20,8 @@ variable "node_size" {
   description = "Size for Azure instances to be used as K8s nodes"
   default     = "Standard_D2_v2"
 }
+
+variable "k8s_base_name" {
+  description = "Name for K8s cluster and resources"
+  default     = "relaxing-parrot" 
+}
