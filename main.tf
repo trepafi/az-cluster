82b35ffc51334fbef57b581fba2b8d6c6e6c9ef7
Lubert Palacios <lubert@MBP-Lubert.local>
2021-09-11 19:53:58 +0200

    Updating backend config

--- main.tf
+++ main.tf
@@ -4,18 +4,9 @@ provider "azurerm" {
   features {}
 }
 
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "${var.resource_group_name}"
-    storage_account_name = "${var.storage_account_name}"
-    container_name       = "${var.tfstate_filename}"
-    key                  = "${var.tfstate_filename}-${var.environment}"
-  }
-}
-
 resource "azurerm_resource_group" "default" {
   name     = "${var.k8s_base_name}-rg"
-  location = "${var.region}"
+  location = var.region
 
   tags = {
     environment = "${var.environment}"
--- variables.tf
+++ variables.tf
@@ -26,17 +26,17 @@ variable "k8s_base_name" {
   default     = "relaxing-parrot"
 }
 
-variable "storage_account_name" {
-  description = "Azure Storage Account name"
-  default = "azk8sstorage"
-}
+# variable "storage_account_name" {
+#   description = "Azure Storage Account name"
+#   default = "azk8sstorage"
+# }
 
-variable resource_group_name {
-  description = "Azure Resource Group name"
-  default = "az-k8s-resource-group"
-}
+# variable resource_group_name {
+#   description = "Azure Resource Group name"
+#   default = "az-k8s-resource-group"
+# }
 
-variable "tfstate_filename" {
-  description = "Terraform state filename"
-  default = "tfstate"
-}
+# variable "tfstate_filename" {
+#   description = "Terraform state filename"
+#   default = "tfstate"
+# }
