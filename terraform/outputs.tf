4d1773f4b8ee04ec1466a9a92293ae805f055e27
Lubert Palacios <lubert@MBP-Lubert.local>
2021-09-18 18:11:28 +0200

    Adding output to master TF

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,10 @@ output "resource_group_name" {
   value = azurerm_resource_group.default.name
 }
 
+output "kubernetes_cluster_name" {
+  value = azurerm_kubernetes_cluster.default.name
+}
+
 # output "host" {
 #   value = azurerm_kubernetes_cluster.default.kube_config.0.host
 # }
