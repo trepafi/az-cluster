e3ce9be8604041a82fd0a9b75b86839e080590a4
Lubert Palacios <lubert@MBP-Lubert.local>
2021-10-03 01:50:54 +0200

    Adding config for Helm provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -12,3 +12,12 @@ provider "kubernetes" {
   client_key             = base64decode(module.aks_cluster.k8s_cluster.kube_config.0.client_key)
   cluster_ca_certificate = base64decode(module.aks_cluster.k8s_cluster.kube_config.0.cluster_ca_certificate)
 }
+
+provider "helm" {
+  kubernetes {
+    host                   = module.aks_cluster.k8s_cluster.kube_config.0.host
+    client_certificate     = base64decode(module.aks_cluster.k8s_cluster.kube_config.0.client_certificate)
+    client_key             = base64decode(module.aks_cluster.k8s_cluster.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(module.aks_cluster.k8s_cluster.kube_config.0.cluster_ca_certificate)
+  }
+}
