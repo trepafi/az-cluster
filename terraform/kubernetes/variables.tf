e197c6b417fe3b0739e24ab39a2087f0c6f71cdc
Lubert Palacios <lubert@MBP-Lubert.local>
2021-10-02 12:52:11 +0200

    Adding variables to K8s module

--- terraform/kubernetes/variables.tf
+++ terraform/kubernetes/variables.tf
@@ -1,6 +1,11 @@
 variable "namespace" {
   type    = string
   default = "dev"
+  description = "Default namespace"
+}
+
+variable "k8s_cluster" {
+  description = "Kubernetes cluster configuration"
 }
 
 # variable "grafana_password" {
