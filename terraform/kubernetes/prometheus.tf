80d36d0a790e091f91f26f068c68d35b2e374014
Lubert Palacios <lubert@MBP-Lubert.local>
2021-10-03 18:20:31 +0200

    Formating tf files

--- terraform/kubernetes/prometheus.tf
+++ terraform/kubernetes/prometheus.tf
@@ -1,9 +1,9 @@
 resource "helm_release" "prometheus" {
   chart      = "prometheus-community/kube-prometheus-stack"
-  # version    = "6.1.11"
   name       = "my-prometheus"
   namespace  = var.metrics_ns
   repository = "https://prometheus-community.github.io/helm-charts/"
+  # version    = "6.1.11"
 
   ## Overriding values
   values = [
