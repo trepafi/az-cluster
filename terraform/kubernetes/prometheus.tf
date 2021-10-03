bd19ad0564638fdf98da4b800aa9e27290543642
Lubert Palacios <lubert@MBP-Lubert.local>
2021-10-03 16:47:13 +0200

    Adding specific version for Kube Prometheus stack

--- terraform/kubernetes/prometheus.tf
+++ terraform/kubernetes/prometheus.tf
@@ -1,5 +1,6 @@
 resource "helm_release" "prometheus" {
-  chart      = "prometheus-community/kube-prometheus-stack:19.0.1"
+  chart      = "prometheus-community/kube-prometheus-stack"
+  version    = "19.0.1"
   name       = "my-prometheus"
   namespace  = var.metrics_ns
   repository = "https://prometheus-community.github.io/helm-charts"
