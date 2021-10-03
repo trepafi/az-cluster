105b1a51889a5db423b94a58bf4481b442375714
Lubert Palacios <lubert@MBP-Lubert.local>
2021-10-03 18:18:00 +0200

    Adding external file for values.yml for kube-prometheus-stack Helm Chart

--- terraform/kubernetes/prometheus.tf
+++ terraform/kubernetes/prometheus.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "prometheus" {
   chart      = "prometheus-community/kube-prometheus-stack"
-  version    = "6.1.11"
+  # version    = "6.1.11"
   name       = "my-prometheus"
   namespace  = var.metrics_ns
   repository = "https://prometheus-community.github.io/helm-charts/"
