0baaa7b4d21bf4e948ad3294a13197d7f3d70367
Lubert Palacios <lubert@MBP-Lubert.local>
2021-10-03 17:34:38 +0200

    Updating TF providers versions

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,17 +2,17 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.66.0"
+      version = "2.79.1"
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.0.3"
+      version = ">= 2.5.0"
     }
 
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.1.0"
+      version = ">= 2.3.0"
     }
   }
 
